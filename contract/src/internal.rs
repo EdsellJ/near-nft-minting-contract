b3954fa3826b6cebad60c0e75bd485603cd8f9b7
Josue Morales <molinajosue92@hotmail.com>
2022-09-02 22:10:34 -0400

    add transfer nft logic

--- contract/src/internal.rs
+++ contract/src/internal.rs
@@ -59,4 +59,69 @@ impl Contract {
         // we insert that set for the given account ID
         self.tokens_per_owner.insert(account_id, &tokens_set);
     }
+
+    pub(crate) fn internal_remove_token_from_owner(
+        &mut self,
+        account_id: &AccountId,
+        token_id: &TokenId
+    ) {
+        // get the set of tokens that the owner has
+        let mut tokens_set = self.tokens_per_owner
+            .get(account_id)
+            .expect("Token should be owned by the sender");
+        
+        // Remove the token_id from the set of tokens
+        tokens_set.remove(token_id);
+
+        // if the token set is now empty, we remove the owner from the tokens_per_ownder collection
+        if tokens_set.is_empty() {
+            self.tokens_per_owner.remove(account_id);
+        } else {
+            // if the token set is not empty, insert it back for the account ID
+            self.tokens_per_owner.insert(account_id, &tokens_set);
+        }
+    }
+
+    pub(crate) fn internal_transfer(
+        &mut self,
+        sender_id: &AccountId,
+        receiver_id: &AccountId,
+        token_id: &TokenId,
+        memo: Option<String>
+    ) -> Token {
+        // Get the token object by passing in the token_id
+        let token = self.tokens_by_id.get(token_id).expect("Not token");
+
+        // If the sender doesnt equal the owner, we panic
+        if sender_id != &token.owner_id {
+            env::panic_str("Unauthorized");
+        }
+
+        // make sure that the sender isn't sending the token to themselves
+        assert_ne!(
+            &token.owner_id, receiver_id,
+            "The token owner and the receiver should be different"
+        );
+
+        // remove the token from its current owner's set
+        self.internal_remove_token_from_owner(&token.owner_id, token_id);
+
+        // add the token to the receiver_id's set
+        self.internal_add_token_to_owner(receiver_id, token_id);
+
+        // create a new token struct
+        let new_token = Token {
+            owner_id: receiver_id.clone()
+        };
+        // insert new token into the tokens_by_id, replacing the old entry
+        self.tokens_by_id.insert(token_id, &new_token);
+
+        // if there was some memo attached, we log it
+        if let Some(memo) = memo {
+            env::log_str(&format!("Memo: {}", memo).to_string());
+        }
+
+        // return the previous token object that was transferred
+        token
+    }
 }
--- contract/src/nft_core.rs
+++ contract/src/nft_core.rs
@@ -1,5 +1,8 @@
 use crate::*;
-use near_sdk::ext_contract;
+use near_sdk::{ext_contract, assert_one_yocto, Gas, PromiseResult, log};
+
+const GAS_FOR_RESOLVE_TRANSFER: Gas = Gas(10_000_000_000_000);
+const GAS_FOR_NFT_ON_TRANSFER: Gas = Gas(25_000_000_000_000);
 
 pub trait NonFungibleTokenCore {
     // Transfers an NFT to a receiver ID
@@ -63,7 +66,19 @@ impl NonFungibleTokenCore for Contract {
         token_id: TokenId,
         memo: Option<String>
     ) {
-        todo!()
+        // assert that the user attached exacly 1 yoctoNEAR, this is for security and so that the user will be
+        // redirected to the NEAR wallet
+        assert_one_yocto();
+        // Get the sender sending the token
+        let sender_id = env::predecessor_account_id();
+
+        // Call the internal transfer method
+        self.internal_transfer(
+            &sender_id,
+            &receiver_id,
+            &token_id,
+            memo
+        );
     }
 
     // Transfers an NFT to a receiver and calls a function ont the receiver ID's contract
@@ -76,7 +91,39 @@ impl NonFungibleTokenCore for Contract {
         memo: Option<String>,
         msg: String,
     ) -> PromiseOrValue<bool> {
-        todo!()
+        // assert that the user attached exactly 1 yocto for security
+        assert_one_yocto();
+
+        // get the sender ID
+        let sender_id = env::predecessor_account_id();
+
+        // transfer the token and get the previous token object
+        let previous_token = self.internal_transfer(
+            &sender_id, 
+            &receiver_id, 
+            &token_id, memo
+        );
+
+        // init receiver's call and the callback
+        // defaulting gas weight to 1, no attached deposit, and static GAS equal to the GAS for nft on transfer
+        ext_non_fungible_token_receiver::ext(receiver_id.clone())
+            .with_static_gas(GAS_FOR_NFT_ON_TRANSFER)
+            .nft_on_transfer(
+                sender_id, 
+                previous_token.owner_id.clone(), 
+                token_id.clone(), 
+                msg
+            )
+            // resolve the promise and call nft_resolve_transfer
+            .then(
+                Self::ext(env::current_account_id())
+                    .with_static_gas(GAS_FOR_RESOLVE_TRANSFER)
+                    .nft_resolve_transfer(
+                        previous_token.owner_id,
+                        receiver_id, 
+                        token_id
+                    )
+            ).into()
     }
 
     // Get info about the NFT token passed in
@@ -108,6 +155,41 @@ impl NonFungibleTokenResolver for Contract {
         receiver_id: AccountId,
         token_id: TokenId,
     ) -> bool {
-        todo!()
+        if let PromiseResult::Successful(value) = env::promise_result(0) {
+            // the nft_on_transfer should return whether we should return the token to its owner or not
+            if let Ok(return_token) = near_sdk::serde_json::from_slice::<bool>(&value) {
+                if !return_token {
+                    return true;
+                }
+            }
+        }
+
+        // get the token object if there is some token obejct
+        let mut token = if let Some(token) = self.tokens_by_id.get(&token_id) {
+            if token.owner_id != receiver_id {
+                // the token is not owned by the receiver anymore
+                return  true;
+            }
+            token
+        } else {
+            return true;
+        };
+
+        // if at the end, we havent returned true, then we should return the token to its original owner
+        log!("Return {} from @{} to @{}", token_id, receiver_id, owner_id);
+
+        // remove the token from the reciever
+        self.internal_remove_token_from_owner(&receiver_id, &token_id);
+
+        // add the token to the original owner
+        self.internal_add_token_to_owner(&owner_id, &token_id);
+
+        // change the token struct's owner to be the original owner
+        token.owner_id = owner_id;
+
+        // insert the token back into the tokens_by_id collection
+        self.tokens_by_id.insert(&token_id, &token);
+
+        false
     }
 }
