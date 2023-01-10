f3e45cd37a5178fe692f8941f3a623a9f357496e
Jacob Edsell <jedsell72@gmail.com>
2023-01-10 13:06:16 -0800

    updated mint to be compatible with the minting page

--- contract/src/mint.rs
+++ contract/src/mint.rs
@@ -5,60 +5,76 @@ use crate::*;
 impl Contract {
     #[payable]
     pub fn nft_mint(
-        &mut self,
-        token_id: TokenId,
-        metadata: TokenMetadata,
-        receiver_id: AccountId,
-        perpetual_royalties: Option<HashMap<AccountId, u32>>,
+        &mut self, role: String, image: String
     ) {
-        // Measture the initial storage being used on the contract
-        let initial_storage_usage = env::storage_usage();
-
-        // create a royalty map to store in the token
-        let mut royalty = HashMap::new();
-
-        // if perpetual royalties were passed into the function:
-        if let Some(perpetual_royalties) = perpetual_royalties {
-            // make sure that the length of the perpetual royalties is below 7
-            assert!(perpetual_royalties.len() < 7, "Cannot add more than 6 perpetual royalty amounts");
-            // iterate through the perpetual royalites and insert the account and amount in the royalty map
-            for (account, amount) in perpetual_royalties {
-                royalty.insert(account, amount);
-            }
+        if (env::block_timestamp() / 1000000) < 1651651200000 {
+            env::panic(b"Not time for WL mint");
         }
-        // specify the token struct that contains the owner ID
-        let token = Token {
-            owner_id: receiver_id,
-            // set the approved account IDs to the default value (an empty map)
-            approved_account_ids: Default::default(),
-            // next approval ID is set to 0
-            next_approval_id: 0,
-            // The map of perpetual royalties for the token (The owner will get 100% - total perpetual royalties)
-            royalty
-        };
 
-        // Insert the token ID and token struct and make sure that the token doesn't exist
-        assert!(
-            self.tokens_by_id.insert(&token_id, &token).is_none(),
-            "Token already exists"
-        );
+        assert!(self.token_metadata_by_id.len() < 100, "Minting ended");
+        let mut royalty = HashMap::new();
+        let account_id = env::predecessor_account_id();
 
-        let mut metadata = metadata.clone();
-        // Check the token type, if None, set equal to TokenType::Content
-        metadata.token_type = match metadata.token_type {
-            None => Some(TokenType::Content),
-            Some(token_type) => Some(token_type)
-        };
+        let amount = env::attached_deposit();
 
-        // Set the issued at time
-        metadata.issued_at = Some(near_sdk::env::block_timestamp_ms());
-        
-        // insert the token ID and metadata
-        self.token_metadata_by_id.insert(&token_id, &metadata);
+        if amount == 4000000000000000000000000 {
+            let initial_storage_usage = env::storage_usage();
+            let token = Token {
+                owner_id: account_id,
+                approved_account_ids: Default::default(),
+                next_approval_id: 0,
+                royalty,
+            };
+            let token_id = (self.token_metadata_by_id.len() + 1).to_string();
 
-        // call the internal method for adding the token to the owner
-        self.internal_add_token_to_owner(&token.owner_id, &token_id);
+            assert!(
+                self.tokens_by_id.insert(&token_id, &token).is_none(),
+                "Token already exists"
+            );
+            let mut title = "".to_string();
+            let mut description = "".to_string();
 
+            title = role.clone() + &"#".to_owned() + &token_id;
+            description = role.clone() + &"NFTs minted by instructor and send to students who passed the study course.".to_owned();
+            
+            
+            self.token_metadata_by_id.insert(&token_id, &TokenMetadata{
+                title: Some(title.to_string()),
+                description: Some(description.to_string()),
+                media: Some(image),
+                media_hash: None,
+                copies: None,
+                issued_at: Some(env::block_timestamp() / 1000000),
+                expires_at: None,
+                starts_at: None,
+                updated_at: None,
+                extra: None,
+                reference: Some(token_id.clone() + ".json"),
+                reference_hash: None
+            });
+            
+            self.internal_add_token_to_owner(&token.owner_id, &token_id);
+            
+            // Construct the mint log as per the events standard.
+            let nft_mint_log: EventLog = EventLog {
+                // Standard name ("nep171").
+                standard: NFT_STANDARD_NAME.to_string(),
+                // Version of the standard ("nft-1.0.0").
+                version: NFT_METADATA_SPEC.to_string(),
+                // The data related with the event stored in a vector.
+                event: EventLogVariant::NftMint(vec![NftMintLog {
+                    // Owner of the token.
+                    owner_id: token.owner_id.to_string(),
+                    // Vector of token IDs that were minted.
+                    token_ids: vec![token_id],
+                    // An optional memo to include.
+                    memo: None,
+                }]),
+            };
+            env::log_str(&nft_mint_log.to_string());
+        } else {
+            env::panic(b"Require correct amount of Near attached");
+        }
         // Construct the mint log as per the events standard.
         let nft_mint_log: EventLog = EventLog {
             // Standard name ("nep171").
