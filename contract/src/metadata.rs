e76a7ce73eac4f884ba93e7488fd9f1e53168948
Jacob Edsell <jedsell72@gmail.com>
2023-01-31 19:33:50 -0800

    updated to minting with series

--- contract/src/metadata.rs
+++ contract/src/metadata.rs
@@ -1,74 +1,73 @@
-use std::collections::HashMap;
-
 use crate::*;
-
-// defines the pyaout type we'll be returning as a part of the royalty standards
+pub type TokenId = String;
+//defines the payout type we'll be returning as a part of the royalty standards.
 #[derive(Serialize, Deserialize)]
 #[serde(crate = "near_sdk::serde")]
 pub struct Payout {
-    pub payout: HashMap<AccountId, U128>
+    pub payout: HashMap<AccountId, U128>,
 }
 
 #[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone)]
 #[serde(crate = "near_sdk::serde")]
 pub struct NFTContractMetadata {
-    pub spec: String,                       // Required
-    pub name: String,                       // Required
-    pub symbol: String,                     // Required
-    pub icon: Option<String>,               // Data URL
-    pub base_uri: Option<String>, 
-    pub reference: Option<String>,
-    pub reference_hash: Option<Base64VecU8>
+    pub spec: String,              // required, essentially a version like "nft-1.0.0"
+    pub name: String,              // required, ex. "Mosaics"
+    pub symbol: String,            // required, ex. "MOSAIC"
+    pub icon: Option<String>,      // Data URL
+    pub base_uri: Option<String>, // Centralized gateway known to have reliable access to decentralized storage assets referenced by `reference` or `media` URLs
+    pub reference: Option<String>, // URL to a JSON file with more info
+    pub reference_hash: Option<Base64VecU8>, // Base64-encoded sha256 hash of JSON from reference field. Required if `reference` is included.
 }
 
-#[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Debug, Clone)]
+#[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize)]
 #[serde(crate = "near_sdk::serde")]
 pub struct TokenMetadata {
-    pub title: Option<String>,
-    pub description: Option<String>,
-    pub media: Option<String>,
-    pub media_hash: Option<String>,
-    pub copies: Option<u64>,
-    pub issued_at: Option<u64>,
-    pub expires_at: Option<u64>,
-    pub starts_at: Option<u64>,
-    pub updated_at: Option<u64>,
-    pub extra: Option<String>,
-    pub reference: Option<String>,
-    pub reference_hash: Option<Base64VecU8>,
-    pub token_type: Option<TokenType>
+    pub title: Option<String>, // ex. "Arch Nemesis: Mail Carrier" or "Parcel #5055"
+    pub description: Option<String>, // free-form description
+    pub media: Option<String>, // URL to associated media, preferably to decentralized, content-addressed storage
+    pub media_hash: Option<Base64VecU8>, // Base64-encoded sha256 hash of content referenced by the `media` field. Required if `media` is included.
+    pub copies: Option<u64>, // number of copies of this set of metadata in existence when token was minted.
+    pub issued_at: Option<u64>, // When token was issued or minted, Unix epoch in milliseconds
+    pub expires_at: Option<u64>, // When token expires, Unix epoch in milliseconds
+    pub starts_at: Option<u64>, // When token starts being valid, Unix epoch in milliseconds
+    pub updated_at: Option<u64>, // When token was last updated, Unix epoch in milliseconds
+    pub extra: Option<String>, // anything extra the NFT wants to store on-chain. Can be stringified JSON.
+    pub reference: Option<String>, // URL to an off-chain JSON file with more info.
+    pub reference_hash: Option<Base64VecU8>, // Base64-encoded sha256 hash of JSON from reference field. Required if `reference` is included.
 }
 
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct Token {
-    // Owner of the token
+    // Series that the token belongs to
+    pub series_id: u64,
+    //owner of the token
     pub owner_id: AccountId,
-
-    // List of approved account IDS that have access to transfer the token.
+    //list of approved account IDs that have access to transfer the token. This maps an account ID to an approval ID
     pub approved_account_ids: HashMap<AccountId, u64>,
-
-    // the next approval ID to give out
+    //the next approval ID to give out.
     pub next_approval_id: u64,
-
-    pub royalty: HashMap<AccountId, u32>
 }
 
-// The Json token is what will be returned from view calls
-#[derive(Serialize, Deserialize, Debug)]
+//The Json token is what will be returned from view calls.
+#[derive(Serialize, Deserialize)]
 #[serde(crate = "near_sdk::serde")]
 pub struct JsonToken {
-    // Token ID
+    // Series that the token belongs to
+    pub series_id: u64,
+    //token ID
     pub token_id: TokenId,
+    //owner of the token
     pub owner_id: AccountId,
+    //token metadata
     pub metadata: TokenMetadata,
-    // List of approved account IDs that have access to transfer the token
+    //list of approved account IDs that have access to transfer the token. This maps an account ID to an approval ID
     pub approved_account_ids: HashMap<AccountId, u64>,
-
-    pub royalty: HashMap<AccountId, u32>
+    //keep track of the royalty percentages for the token in a hash map
+    pub royalty: Option<HashMap<AccountId, u32>>,
 }
 
 pub trait NonFungibleTokenMetadata {
-    // View call for returning the contract metadata
+    //view call for returning the contract metadata
     fn nft_metadata(&self) -> NFTContractMetadata;
 }
 
@@ -77,11 +76,4 @@ impl NonFungibleTokenMetadata for Contract {
     fn nft_metadata(&self) -> NFTContractMetadata {
         self.metadata.get().unwrap()
     }
-}
-
-#[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize, Debug, Clone)]
-#[serde(crate = "near_sdk::serde")]
-pub enum TokenType {
-    Content,
-    Badge
 }
